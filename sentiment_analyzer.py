7c6255b160e9e841d0d84c2623387ecdf175beef
Ritankar Mondal <ritankarmondal123@gmail.com>
2025-06-08 13:23:52 +0530

    cs

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -6,7 +6,6 @@ def load_classifier():
     tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
     model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert")
     return pipeline("text-classification", model=model, tokenizer=tokenizer)
-
 def analyze_sentiment_batch(classifier, texts):
     results = classifier(texts, truncation=True)
     return [(r['label'], r['score']) for r in results]
